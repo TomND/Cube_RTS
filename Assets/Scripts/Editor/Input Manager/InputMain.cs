33d4ea95f6dfb775f87bdb27b37bf4d250680888
Daniel-T-Horga <tom-horga@hotmail.com>
2016-04-20 15:59:41 -0400

    Added Camera Controller
    - added camera controller
    - created a scene
    - changed some values in Input manager

---
+++ Assets/Scripts/Editor/Input Manager/InputMain.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class InputMain : EditorWindow {
+   // Add menu item named "My Window" to the Window menu
+   [MenuItem("Window/Input Manager")]
+   public static void ShowWindow()
+   {
+      //Show existing window instance. If one doesn't exist, make one.
+      EditorWindow.GetWindow(typeof(InputMain));
+   }
+
+   void OnGUI()
+   {
+      GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+      GUILayout.TextField("test", 25);
+   }
+
+   // Use this for initialization
+   void Start()
+   {
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+   }
+}
